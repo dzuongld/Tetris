413cf4d34b769e707331ff445ebeadd3deabd4a0
dzuongld <dzuongld@gmail.com>
2017-12-20 22:58:33 +1100

    Initial commit

---
+++ Assets/BlockController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    enum State
+    {
+        Free, HitLeft, HitRight, HitBottom
+    }
+    State state = State.Free;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+        Fall();
+    }
+
+    void HandleInput()
+    {
+        if (state == State.HitBottom)
+        {
+            this.tag = "Bottom";
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (state == State.HitRight)
+                state = State.Free;
+            if (state != State.HitLeft)
+                transform.position = transform.position + new Vector3(-2, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (state == State.HitLeft)
+                state = State.Free;
+            if (state != State.HitRight)
+                transform.position = transform.position + new Vector3(2, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Rotate(Vector3.back, 90f);
+        }
+    }
+
+    private void Fall()
+    {
+        if (state != State.HitBottom)
+            transform.position = transform.position + new Vector3(0, -0.1f, 0);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bottom")
+        {
+            state = State.HitBottom;
+            return;
+        }
+        if (collision.gameObject.tag == "Left")
+        {
+            state = State.HitLeft;
+        }
+        if (collision.gameObject.tag == "Right")
+        {
+            state = State.HitRight;
+        }
+
+    }
+}
