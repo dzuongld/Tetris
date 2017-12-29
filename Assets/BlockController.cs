3582390961c3b812b052d6aa6d8347f3ef019a81
dzuongld <dzuongld@gmail.com>
2017-12-29 12:44:57 +1100

    Improvements
    -Rotation fixed
    -Upcoming block shown
    -Game can end properly

--- Assets/BlockController.cs
+++ Assets/BlockController.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 
 public class BlockController : MonoBehaviour
 {
-    [SerializeField] int speedFactor = 1;
+    [SerializeField] float speedFactor = 0.1f;
 
     Rigidbody rb;
-    Vector3 fallDirection = new Vector3(0, -0.01f, 0);
+    Vector3 fallDirection = new Vector3(0, -1f, 0);
     bool isReady = false;
+    GameController gameController;
 
     enum State
     {
@@ -15,19 +17,18 @@ public class BlockController : MonoBehaviour
     }
     State state = State.Free;
 
-    //public Transform spawnPoint;
-
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        gameController = GameObject.Find("SpawnPoint").GetComponent<GameController>();
+        InvokeRepeating("Fall", 0, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         HandleInput();
-        Fall();
     }
 
     void HandleInput()
@@ -39,50 +40,55 @@ public class BlockController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            if (state == State.HitRight)
-                state = State.Free;
-            if (state != State.HitLeft)
+            if (state != State.HitLeft && OkToMove("left"))
+            {
                 transform.position = transform.position + new Vector3(-1, 0, 0);
-            //CheckChildren();
-
+                if (state == State.HitRight)
+                    state = State.Free;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            if (state == State.HitLeft)
-                state = State.Free;
-            if (state != State.HitRight)
+            if (state != State.HitRight && OkToMove("right"))
+            {
                 transform.position = transform.position + new Vector3(1, 0, 0);
-            //CheckChildren();
-
+                if (state == State.HitLeft)
+                    state = State.Free;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.W))
+        else if (Input.GetKeyDown(KeyCode.W) && OkToMove("rotate"))
         {
             transform.Rotate(Vector3.back, 90f);
-            //CheckChildren();
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            speedFactor = 10;
+            CancelInvoke("Fall");
+            InvokeRepeating("Fall", 0, 0.8f);
         }
         else if (Input.GetKeyUp(KeyCode.S))
         {
-            speedFactor = 1;
+            CancelInvoke("Fall");
+            InvokeRepeating("Fall", 0, 1f);
         }
+
+        int[] positions = ProcessPosition();
+        print(string.Join(" ", positions.Select(x => x.ToString()).ToArray()));
 
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        //print(ReturnDirection(collision.gameObject, this.gameObject) + this.gameObject.tag);
-        print(HitSide(collision));
 
-        foreach (ContactPoint contact in collision.contacts)
-        {
-            print("Contact " + contact.point.x + " " + contact.point.y + " " + contact.point.z);
-        }
+        //foreach (ContactPoint contact in collision.contacts)
+        //{
+        //    print("Contact " + contact.point.x + " " + contact.point.y + " " + contact.point.z);
+        //}
+
+        //print(HitSide(collision));
 
         if (collision.gameObject.tag == "Bottom" || (collision.gameObject.tag == "BottomBlock" && !HitSide(collision)))
         {
+            //print(this.tag);
             HandleDrop();
             return;
         }
@@ -97,6 +103,14 @@ public class BlockController : MonoBehaviour
 
     }
 
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "BottomBlock" && !HitSide(collision))
+        {
+            HandleDrop();
+        }
+    }
+
     public bool IsReady()
     {
         if (isReady)
@@ -105,6 +119,12 @@ public class BlockController : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    private void Fall()
+    {
+        if (state != State.HitBottom)
+            transform.position = transform.position + fallDirection;
     }
 
     private void HandleDrop()
@@ -118,19 +138,6 @@ public class BlockController : MonoBehaviour
         rb.isKinematic = true;
         FixPosition();
         isReady = true;
-
-        ClearRows();
-    }
-
-    private void Fall()
-    {
-        if (state != State.HitBottom)
-        {
-            //rb.isKinematic = true;
-            transform.position = transform.position + fallDirection * speedFactor;
-            //rb.isKinematic = false;
-        }
-        //transform.GetChild(0).
     }
 
     //determine if current block has hit the side of a dropped block
@@ -143,35 +150,43 @@ public class BlockController : MonoBehaviour
         //regular collision
         if (collision.contacts.Length == 4)
         {
-            //pick one point
-            ContactPoint p1 = collision.contacts[0];
-            //print("Point " + p1.point.x + " " + p1.point.y + " " + p1.point.z);
-            for (int i = 1; i < 4; i++)
+            for (int i = 0; i < 4; i++)
             {
-                ContactPoint p2 = collision.contacts[i];
-
-                //find the other point on the same x axis that shares y and z coords
-                if (Mathf.Abs(p1.point.y - p2.point.y) <= Mathf.Epsilon && Mathf.Abs(p1.point.z - p2.point.z) <= Mathf.Epsilon)
+                //pick one point
+                ContactPoint p1 = collision.contacts[i];
+                //print("Point1 " + p1.point.x + " " + p1.point.y + " " + p1.point.z);
+                for (int j = 0; j < 4; j++)
                 {
-                    //print("Point " + p2.point.x + " " + p2.point.y + " " + p2.point.z);
-                    //ideally should work with < 1 
-                    if (Mathf.Abs(p1.point.x - p2.point.x) < 0.5)
+                    if (j != i)
                     {
-                        //print(p1.point.z - p2.point.z);
-                        //print("Point1 H " + p1.point.x + " " + p1.point.y + " " + p1.point.z);
-                        //print("Point2 H " + p2.point.x + " " + p2.point.y + " " + p2.point.z);
-                        return true;
-                    }
-                }
+                        ContactPoint p2 = collision.contacts[j];
 
-                //vertical collision
-                if (Mathf.Abs(p1.point.x - p2.point.x) <= Mathf.Epsilon && Mathf.Abs(p1.point.z - p2.point.z) <= Mathf.Epsilon)
-                {
-                    if (Mathf.Abs(p1.point.y - p2.point.y) > 0)
-                    {
-                        //print("Point2 V " + p1.point.x + " " + p1.point.y + " " + p1.point.z);
-                        //print("Point2 V " + p2.point.x + " " + p2.point.y + " " + p2.point.z);
-                        return true;
+                        //find the other point on the same x axis that shares y and z coords
+                        if (Mathf.Abs(p1.point.y - p2.point.y) < 0.5 &&
+                            Mathf.Abs(p1.point.z - p2.point.z) < 0.5)
+                        {
+                            //print("Point2 " + p2.point.x + " " + p2.point.y + " " + p2.point.z);
+                            //ideally should work with < 1 
+                            if (Mathf.Abs(p1.point.x - p2.point.x) < 0.5)
+                            {
+                                //print(p1.point.z - p2.point.z);
+                                //print("Point1 H " + p1.point.x + " " + p1.point.y + " " + p1.point.z);
+                                //print("Point2 H " + p2.point.x + " " + p2.point.y + " " + p2.point.z);
+                                return true;
+                            }
+                        }
+
+                        //vertical collision
+                        if (Mathf.Abs(p1.point.x - p2.point.x) < 0.5 &&
+                            Mathf.Abs(p1.point.z - p2.point.z) < 0.5)
+                        {
+                            if (Mathf.Abs(p1.point.y - p2.point.y) > 0.5)
+                            {
+                                //print("Point2 V " + p1.point.x + " " + p1.point.y + " " + p1.point.z);
+                                //print("Point2 V " + p2.point.x + " " + p2.point.y + " " + p2.point.z);
+                                return true;
+                            }
+                        }
                     }
                 }
             }
@@ -194,35 +209,147 @@ public class BlockController : MonoBehaviour
         transform.position = new Vector3(x, y, 0);
     }
 
-    private void CheckChildren()
+    private bool OkToMove(string side)
     {
-        float minX = float.MaxValue, minY = float.MaxValue;
-        float maxX = float.MinValue, maxY = float.MinValue;
+        int[] positions = ProcessPosition();
+        int leftSpaces = positions[0];
+        int rightSpaces = positions[1];
+        int distanceToTop = positions[2];
+        int distanceToBottom = positions[3];
+
+        switch (side)
+        {
+            case "left":
+                return leftSpaces > 0;
+            case "right":
+                return rightSpaces > 0;
+            case "rotate":
+                if ((rightSpaces >= distanceToTop) && (leftSpaces >= distanceToBottom))
+                {
+                    if (state == State.HitRight && distanceToTop <= rightSpaces)
+                        state = State.Free;
+
+                    if (state == State.HitLeft && distanceToBottom <= leftSpaces)
+                        state = State.Free;
+
+                    return true;
+                }
+
+                if (distanceToTop > rightSpaces)
+                {
+                    int neededSpaces = distanceToTop - rightSpaces;
+                    int availableSpaces = leftSpaces - distanceToBottom;
+
+                    if (availableSpaces >= neededSpaces)
+                    {
+                        transform.position = transform.position + new Vector3(-1 * neededSpaces, 0, 0);
+                        if (state == State.HitRight)
+                            state = State.Free;
+
+                        return true;
+                    }
+
+                }
+
+                if (distanceToBottom > leftSpaces)
+                {
+                    int neededSpaces = distanceToBottom - leftSpaces;
+                    int availableSpaces = rightSpaces - distanceToTop;
+
+                    if (availableSpaces >= neededSpaces)
+                    {
+                        transform.position = transform.position + new Vector3(1 * neededSpaces, 0, 0);
+                        if (state == State.HitLeft)
+                            state = State.Free;
+
+                        return true;
+                    }
+
+                }
+
+                return false;
+            default:
+                return false;
+        }
+    }
+
+    private int[] ProcessPosition()
+    {
+        var center = transform.GetChild(0);
+        float centerY = center.position.y;
+
+        float bottomY = float.MaxValue, leftX = float.MaxValue;
+        float topY = float.MinValue, rightX = float.MinValue;
+        float leftY = 0, rightY = 0;
 
         for (int i = 0; i < transform.childCount; i++)
         {
             var child = transform.GetChild(i);
-            print(i + " " + child.transform.position.x + " " + child.transform.position.y + " " + child.transform.position.z);
+            //print(i + " " + child.transform.position.x + " " + child.transform.position.y + " " +
+            //child.transform.position.z);
 
-            if (minX < child.transform.position.x)
-                minX = child.transform.position.x;
-            if (maxX > child.transform.position.x)
-                maxX = child.transform.position.x;
-            if (minY < child.transform.position.y)
-                minY = child.transform.position.y;
-            if (maxY < child.transform.position.y)
-                maxY = child.transform.position.y;
+            if (leftX > child.transform.position.x)
+            {
+                leftX = child.transform.position.x;
+                leftY = child.transform.position.y;
+            }
+            if (rightX < child.transform.position.x)
+            {
+                rightX = child.transform.position.x;
+                rightY = child.transform.position.y;
+            }
+            if (bottomY > child.transform.position.y)
+            {
+                bottomY = child.transform.position.y;
+            }
+            if (topY < child.transform.position.y)
+            {
+                topY = child.transform.position.y;
+            }
+        }
+
+        int[,] map = gameController.GetMap();
+        int cols = gameController.GetColCount();
+
+        int x = (int)(Round(leftX) + 5.5f);
+        int y = (int)(Round(leftY) - 0.5f);
+        int index = x - 1;
+
+        int leftSpaces = 0;
+        while (index >= 0 && map[y, index] != 1)
+        {
+            leftSpaces++;
+            index--;
+        }
+
+        x = (int)(Round(rightX) + 5.5f);
+        y = (int)(Round(rightY) - 0.5f);
+        index = x + 1;
+
+        int rightSpaces = 0;
+        while (index <= cols - 1 && map[y, index] != 1)
+        {
+            rightSpaces++;
+            index++;
+        }
+
+        int distanceToTop = (int)Round(topY - centerY);
+        int distanceToBottom = (int)Round(centerY - bottomY);
+
+        return new int[] { leftSpaces, rightSpaces, distanceToTop, distanceToBottom };
+    }
+
+    public void UpdateMap(int[,] blockMap)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            int column = (int)(Round(child.transform.position.x) + 5.5f);
+            int row = (int)(Round(child.transform.position.y) - 0.5f);
+
+            blockMap[row, column] = 1;
         }
     }
 
-    private void ClearRows()
-    {
-        print(transform.position.y);
-        DestroyObject(transform.GetChild(0).gameObject);
-        DestroyObject(transform.GetChild(3).gameObject);
-        //transform.GetChild(1).gameObject.transform.position = transform.GetChild(1).gameObject.transform.position + new Vector3(0, -0.5f, 0);
-        //transform.GetChild(2).gameObject.transform.position = transform.GetChild(2).gameObject.transform.position + new Vector3(0, -0.5f, 0);
-        transform.position = transform.position + new Vector3(0, -1f, 0);
-        print(transform.position.y);
-    }
+
 }
---
+++ Assets/GameController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject[] prefabs;
+    GameObject comingBlock;
+    GameObject currentBlock;
+    BlockController script;
+    int index;
+    Vector3 demoPoint = new Vector3(15, 15, 0);
+
+    int maxRows = 26;
+    int maxCols = 12;
+    int[,] blockMap;
+    List<GameObject> blockList = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+        blockMap = new int[maxRows, maxCols];
+        currentBlock = (GameObject)Instantiate(prefabs[0], transform.position, transform.rotation);
+        script = currentBlock.GetComponent<BlockController>();
+
+        index = Random.Range(0, 6);
+        comingBlock = (GameObject)Instantiate(prefabs[index], demoPoint, Quaternion.identity);
+        comingBlock.GetComponent<BlockController>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameOver())
+        {
+            return;
+        }
+        if (script.IsReady())
+        {
+            script.UpdateMap(blockMap);
+            PrintMap();
+            ClearRows();
+
+            currentBlock = (GameObject)Instantiate(prefabs[index], transform.position, transform.rotation);
+            script = currentBlock.GetComponent<BlockController>();
+            blockList.Add(currentBlock);
+
+            if (comingBlock != null)
+                Destroy(comingBlock);
+            index = Random.Range(0, 6);
+            comingBlock = (GameObject)Instantiate(prefabs[index], demoPoint, Quaternion.identity);
+            comingBlock.GetComponent<BlockController>().enabled = false;
+        }
+
+    }
+
+    private bool GameOver()
+    {
+        var mid = maxCols % 2 == 1 ? (maxCols - 1) / 2 : maxCols / 2;
+        return blockMap[maxRows - 1, mid] == 1;
+    }
+
+    public int[,] GetMap()
+    {
+        return blockMap;
+    }
+
+    public int GetRowCount()
+    {
+        return maxRows;
+    }
+
+    public int GetColCount()
+    {
+        return maxCols;
+    }
+
+    private void PrintMap()
+    {
+        for (int i = maxRows - 1; i >= 0; i--)
+        {
+            string row = "";
+            for (int j = 0; j < maxCols; j++)
+            {
+                row += blockMap[i, j] + " ";
+            }
+            print(row);
+        }
+    }
+
+    private void ClearRows()
+    {
+        //print(transform.position.y);
+        //DestroyObject(transform.GetChild(0).gameObject);
+        //DestroyObject(transform.GetChild(3).gameObject);
+        //transform.GetChild(1).gameObject.transform.position = transform.GetChild(1).gameObject.transform.position + new Vector3(0, -0.5f, 0);
+        //transform.GetChild(2).gameObject.transform.position = transform.GetChild(2).gameObject.transform.position + new Vector3(0, -0.5f, 0);
+        //transform.position = transform.position + new Vector3(0, -1f, 0);
+        //print(transform.position.y);
+
+        for (int i = 0; i < maxRows; i++)
+        {
+            bool rowFull = true;
+            for (int j = 0; j < maxCols; j++)
+            {
+                if (blockMap[i, j] == 0)
+                {
+                    rowFull = false;
+                    break;
+                }
+            }
+            if (rowFull)
+            {
+
+            }
+        }
+    }
+
+}
